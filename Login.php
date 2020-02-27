7790cefc4a50120946efbdc0da15a3f997e27af9
uortiz <uortiz@sorint.it>
2020-02-27 19:37:45 +0100

    First commit

---
+++ Login.php
@@ -0,0 +1,14 @@
+<?php include  'database.php' ?>
+
+<?php
+
+if(isset($_POST['login'])) {
+    echo "tutto ok";
+}else {
+    echo "non ok";
+}
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include  'database.php' ?>
+
+<html>
+<head>
+    <title>Login/Logout</title>
+</head>
+<body>
+
+    <form action="Login.php" method="post">
+
+        <input type="text" name="username">
+
+        <input type="password" name="password">
+
+        <button type="submit" name="login">Access</button>
+
+    </form>
+
+</body>
+</html>
