3405600837e47f9100c16ce8101ee0b5bc6bb5d0
uortiz <uortiz@sorint.it>
2020-02-27 20:33:55 +0100

    fixed

--- database.php
+++ database.php
@@ -1,11 +1,16 @@
 <?php
+
     $server = "eu-cdbr-west-02.cleardb.net/heroku_36d91ccb9e97f8f";
     $username = "bf4762acadbdf4";
     $password = "48baa9aa";
     $database = "heroku_36d91ccb9e97f8f";
 
-    $connection = new mysqli($server, $username, $password, $database);
+    $connection = mysqli_connect($server, $username, $password, $database);
 
+    // Check connection
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
     echo "Connected successfully";
 
 
